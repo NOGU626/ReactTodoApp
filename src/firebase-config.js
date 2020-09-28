46c2e5b587fa7b05c8eafe270927332d976faab2
NOGU.D <44973072+NOGU626@users.noreply.github.com>
2020-09-28 19:38:00 +0900

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,8 @@
 export default {
-    apiKey: "AIzaSyD7fiIrydOiDAAv5i_FqQW6DfNnVpCyJ5k",
-    authDomain: "react-store-c199c.firebaseapp.com",
-    databaseURL: "https://react-store-c199c.firebaseio.com",
-    projectId: "react-store-c199c",
-    storageBucket: "react-store-c199c.appspot.com",
-    messagingSenderId: "984477939262",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
